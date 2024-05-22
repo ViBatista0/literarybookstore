6d339a46df61c47d7f5ee2f212cbcea5fdfda708
Vinícius Batista <112179061+ViBatista0@users.noreply.github.com>
2024-05-22 20:58:02 -0300

    commit: Criação de relacionamento entre entidades

---
+++ src/main/java/dev/vibatista/literarybookstore/LiterarybookstoreApplication.java
@@ -0,0 +1,13 @@
+package dev.vibatista.literarybookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterarybookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterarybookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vibatista/literarybookstore/LiterarybookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vibatista.literarybookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterarybookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
