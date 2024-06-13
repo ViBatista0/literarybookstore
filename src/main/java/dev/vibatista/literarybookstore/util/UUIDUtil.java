cf62a86a881533cb2a0736f9f8ca489dd1f70cfb
Vinícius Batista <112179061+ViBatista0@users.noreply.github.com>
2024-06-13 15:54:09 -0300

    commit: Create method to validate UUID

---
+++ src/main/java/dev/vibatista/literarybookstore/util/UUIDUtil.java
@@ -0,0 +1,21 @@
+package dev.vibatista.literarybookstore.util;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+    // É static porque não depende do estado da classe, ele funciona de um jeito, independente de qualquer coisa
+    public static boolean isValidUUID(String uuid){
+        if(uuid == null|| uuid.isEmpty())
+            return false;
+        try{
+            UUID id = UUID.fromString(uuid);
+            return true;
+        }
+        catch (IllegalArgumentException ex){
+            return false;
+        }
+    }
+
+
+}
