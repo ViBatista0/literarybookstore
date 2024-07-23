28bdee73ce8e3227270f885534b1a4be9baf2948
Vinícius Batista <112179061+ViBatista0@users.noreply.github.com>
2024-07-23 11:07:16 -0300

    commit: Delete carrinho repository

--- src/main/java/dev/vibatista/literarybookstore/domain/repositories/carrinho/ItemCarrinhoRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.vibatista.literarybookstore.domain.repositories.carrinho;
-
-import dev.vibatista.literarybookstore.domain.models.carrinho.ItemCarrinho;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, UUID> {
-}
